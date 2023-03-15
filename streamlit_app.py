f4a6b0359ff153e6726d3dba45886db3a44b586c
sarahwarren0922 <101750224+sarahwarren0922@users.noreply.github.com>
2023-03-15 18:24:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 # Display title and text
 st.title("Amsterdam AirBnB - Data and Visualization (Python for DS Week 1)")
-st.markdown("Here we can see the dataframe created during this weeks project.")
+st.markdown("In this project we pre-processed, cleaned, and manipulated raw data from Amsterdam AirBnB to produce the below map. First, here is the dataframe created for this project.")
 
 # Read dataframe
 dataframe = pd.read_csv(
@@ -24,17 +24,17 @@ dataframe = pd.read_csv(
 dataframe = dataframe[dataframe["Price"] <= 100]
 
 # Display as integer
-dataframe["Airbnb Listing ID"] = dataframe["Airbnb Listing ID"].astype(int)
+dataframe["Airbnb Listing ID"] = dataframe["Airbnb Listing ID"].astype(str)
 # Round of values
 dataframe["Price"] = "£ " + dataframe["Price"].round(2).astype(str) # <--- CHANGE THIS POUND SYMBOL IF YOU CHOSE CURRENCY OTHER THAN POUND
 # Rename the number to a string
 dataframe["Location"] = dataframe["Location"].replace(
-    {1.0: "To visit", 0.0: "Airbnb listing"}
+    {1.0: "Hortus Botanical Gardens", 0.0: "Airbnb listing"}
 )
 
 # Display dataframe and text
 st.dataframe(dataframe)
-st.markdown("Below is a map showing all the Airbnb listings with a red dot and the location we've chosen with a blue dot.")
+st.markdown("Now, here is a map showing all the Airbnb listings with a light-blue dot and the Hortus Botanical Gardens with a dark-blue dot.")
 
 # Create the plotly express figure
 fig = px.scatter_mapbox(
@@ -46,7 +46,7 @@ fig = px.scatter_mapbox(
     height=500,
     width=800,
     hover_name="Price",
-    hover_data=["Meters from chosen location", "Location"],
+    hover_data=["Meters from Gardens", "Location"],
     labels={"color": "Locations"},
 )
 fig.update_geos(center=dict(lat=dataframe.iloc[0][2], lon=dataframe.iloc[0][3]))
